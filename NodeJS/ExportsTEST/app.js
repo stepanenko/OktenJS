6609cb0b3908d8ca2aa3dec6f17aa73f97737235
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2018-09-26 18:23:44 +0300

    new

---
+++ NodeJS/ExportsTEST/app.js
@@ -0,0 +1,23 @@
+class Car {
+  constructor(make, year) {
+    this.make = make;
+    this.year = year;
+  }
+  info() {
+    console.log(`Car make: ${this.make}, released in ${this.year}`)
+  }
+}
+
+function Cars (make, year, volume){
+  this.make = make;
+  this.year = year;
+  this.volume = volume;
+};
+Cars.prototype = function info() {
+  console.log(`Car make: ${this.make}, released in ${this.year}`)
+}
+
+const mazda = new Cars('mazda', 2019, 2)
+//console.log(mazda);
+
+module.exports = Cars;
---
+++ NodeJS/ExportsTEST/doc.js
@@ -0,0 +1,6 @@
+const Ca = require('./app');
+//console.log(Ca)
+
+const renault = new Ca('Renault', 2018, 3)
+console.log(renault)
+
