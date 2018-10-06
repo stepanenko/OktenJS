78223464b1b77679b6f74661818d4e8b2b14926f
stepserg <serhiy.stepanenko@aiesec.net>
2018-10-07 00:08:52 +0300

    video pug

---
+++ Video/Pug/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use('/', (req, res, next) => {
+  console.log('Middleware used...')
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+
+app.listen(3000, () => console.log('Listening...'));
